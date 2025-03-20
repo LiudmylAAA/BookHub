2dde523320972647b30534fc79ef0c0cf61a9c63
Myroslavss <myroslava.tkachuk.6@gmail.com>
2025-03-20 20:57:45 +0200

    Update BookHub_dataBase.sql

--- BookHub_dataBase.sql
+++ BookHub_dataBase.sql
@@ -1,6 +1,3 @@
-create database bookhubbd;
-drop database  bookhubbd;
-use bookhubbd;
 
 create table if not exists categories (
     category_id int auto_increment primary key,
@@ -10,8 +7,8 @@ create table if not exists categories (
 create table if not exists genres (
     genre_id int auto_increment primary key,
     name varchar(255) not null,
-    category_id int not null,
-    foreign key (category_id) references genre(category_id) on delete set null
+    category_id int default null,
+    foreign key (category_id) references categories(category_id) on delete cascade on update cascade
 );
 
 create table if not exists publisher (
@@ -20,19 +17,10 @@ create table if not exists publisher (
 );
 
 create table if not exists authors (
-    authors_id int auto_increment primary key,
+    author_id int auto_increment primary key,
     nickname varchar(255),
     first_name varchar(255) not null,
     last_name varchar(255) not null
-);
-
-create table if not exists customers (
-    customer_id int auto_increment primary key,
-    first_name varchar(255) not null,
-    last_name varchar(255) not null,
-    email varchar(255) not null,
-    phone_number varchar(20) not null,
-    password varchar(255) not null
 );
 
 create table if not exists books (
@@ -43,34 +31,25 @@ create table if not exists books (
     genre_id int,
     publisher_id int,
     author_id int not null,
-    foreign key (categories_id) references categories(categories_id) on delete set null,
+    foreign key (genre_id) references genres(genre_id) on delete set null,
     foreign key (publisher_id) references publisher(publisher_id) on delete set null,
-    foreign key (author_id) references authors(authors_id) on delete cascade
+    foreign key (author_id) references authors(author_id) on delete cascade
 );
 
-create table if not exists order_items (
-    order_item_id int auto_increment primary key,
-    order_id int not null,
-    book_id int not null,
-    quantity int not null,
-    unit_price decimal(10,2) not null,
-    foreign key (order_id) references orders(order_id) on delete cascade on update cascade,
-    foreign key ( book_id) references tovar( book_id) on delete no action on update cascade
+create table if not exists customers (
+    customer_id int auto_increment primary key,
+    first_name varchar(255) not null,
+    last_name varchar(255) not null,
+    email varchar(255) not null unique,
+    phone_number varchar(20) not null,
+    password varchar(255) not null check (length(password) >= 8)
 );
-create table if not exists payments (
-    payment_id int auto_increment primary key,
-    order_id int not null,
-    payment_date datetime not null,
-    amount decimal(10,2) not null,
-    payment_method enum('онлайн-оплата', 'накладний платіж') not null,
-    foreign key (order_id) references orders(order_id) on delete cascade on update cascade
-);
+
 create table if not exists cities (
     city_id int auto_increment primary key,
     name varchar(255) not null unique
 );
 
--- Таблиця відділень Нової Пошти
 create table if not exists branches (
     branch_id int auto_increment primary key,
     city_id int not null,
@@ -78,7 +57,7 @@ create table if not exists branches (
     foreign key (city_id) references cities(city_id) on delete cascade
 );
 
-create table orders (
+create table if not exists orders (
     order_id int auto_increment primary key,
     customer_id int,
     city_id int not null,
@@ -91,4 +70,21 @@ create table orders (
     foreign key (branch_id) references branches(branch_id) on delete restrict on update cascade
 );
 
+create table if not exists order_items (
+    order_item_id int auto_increment primary key,
+    order_id int not null,
+    book_id int not null,
+    quantity int not null,
+    unit_price decimal(10,2) not null,
+    foreign key (order_id) references orders(order_id) on delete cascade on update cascade,
+    foreign key (book_id) references books(book_id) on delete cascade on update cascade
+);
 
+create table if not exists payments (
+    payment_id int auto_increment primary key,
+    order_id int not null,
+    payment_date datetime not null,
+    amount decimal(10,2) not null,
+    payment_method enum('онлайн-оплата', 'накладний платіж') not null,
+    foreign key (order_id) references orders(order_id) on delete cascade on update cascade
+);
